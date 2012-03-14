TST: new test suite for precision recall and average_precision functions. Some bug fixes too.

bangmetric/precision_recall.py
@@ -10,13 +10,14 @@ http://en.wikipedia.org/wiki/Precision_and_recall
 __all__ = ['precision', 'recall', 'average_precision']
 
 
+from warnings import warn as warning
 import numpy as np
 from scipy.integrate import trapz
 
 DTYPE = np.float64
 
 
-def precision(y_true, y_pred):
+def precision(y_true, y_pred, argsort_kind='quicksort'):
     """Computes the Precision values w.r.t. descending `y_pred` values.
 
     Parameters
@@ -29,6 +30,9 @@ def precision(y_true, y_pred):
     y_pred: array, shape = [n_samples]
         Predicted values.
 
+    argsort_kind: str
+        Sorting algorithm.
+
     Returns
     -------
     prec: array, shape = [n_samples]
@@ -40,17 +44,26 @@ def precision(y_true, y_pred):
     y_true = np.array(y_true, dtype=DTYPE)
     y_pred = np.array(y_pred, dtype=DTYPE)
 
-    idx = (-y_pred).argsort()
+    n_uniques = np.unique(y_pred)
+    if n_uniques.size == 1:
+        raise ValueError('Rank of predicted values is ill-defined'
+                         ' because all elements are equal')
+    elif n_uniques.size < y_pred.size:
+        warning('some predicted elements have exactly the same value.'
+                ' output will most probably depend on the sorting'
+                ' method used. Here "%s"' % argsort_kind)
 
-    tp = (y_true[idx] > 0).cumsum()
-    fp = (y_true[idx] <= 0).cumsum()
+    idx = (-y_pred).argsort(kind=argsort_kind)
+
+    tp = (y_true[idx] > 0).cumsum(dtype=DTYPE)
+    fp = (y_true[idx] <= 0).cumsum(dtype=DTYPE)
 
     prec = tp / (fp + tp)
 
     return prec
 
 
-def recall(y_true, y_pred):
+def recall(y_true, y_pred, argsort_kind='quicksort'):
     """Computes the Recall values w.r.t. descending `y_pred` values.
 
     Parameters
@@ -63,6 +76,9 @@ def recall(y_true, y_pred):
     y_pred: array, shape = [n_samples]
         Predicted values.
 
+    argsort_kind: str
+        Sorting algorithm.
+
     Returns
     -------
     rec: array, shape = [n_samples]
@@ -74,11 +90,20 @@ def recall(y_true, y_pred):
     y_true = np.array(y_true, dtype=DTYPE)
     y_pred = np.array(y_pred, dtype=DTYPE)
 
-    idx = (-y_pred).argsort()
+    n_uniques = np.unique(y_pred)
+    if n_uniques.size == 1:
+        raise ValueError('Rank of predicted values is ill-defined'
+                         ' because all elements are equal')
+    elif n_uniques.size < y_pred.size:
+        warning('some predicted elements have exactly the same value.'
+                ' output will most probably depend on the sorting'
+                ' method used. Here "%s"' % argsort_kind)
 
-    tp = (y_true[idx] > 0).cumsum()
+    idx = (-y_pred).argsort(kind=argsort_kind)
 
-    y_true_n_pos = (y_true > 0).sum()
+    tp = (y_true[idx] > 0).cumsum(dtype=DTYPE)
+
+    y_true_n_pos = (y_true > 0).sum(dtype=DTYPE)
     if y_true_n_pos == 0:
         rec = np.zeros(tp.shape, dtype=DTYPE)
     else:
@@ -87,7 +112,8 @@ def recall(y_true, y_pred):
     return rec
 
 
-def average_precision(y_true, y_pred, integration='trapz'):
+def average_precision(y_true, y_pred, integration='trapz',
+                      argsort_kind='quicksort'):
     """Computes the Average Precision (AP) from the recall and precision
     arrays. Different 'integration' methods can be used.
 
@@ -107,10 +133,19 @@ def average_precision(y_true, y_pred, integration='trapz'):
             'voc2010': see http://goo.gl/glxdO and http://goo.gl/ueXzr
             'voc2007': see http://goo.gl/E1YyY
 
+    argsort_kind: str
+        Sorting algorithm.
+
     Returns
     -------
     ap: float
         Average Precision
+
+    Note
+    ----
+    'voc2007' method is here only for legacy purposes. We do not recommend
+    its use since even simple trivial cases like a perfect match between
+    true values and predicted values do not lead to an average precision of 1.
     """
 
     assert len(y_true) == len(y_pred)
@@ -119,8 +154,17 @@ def average_precision(y_true, y_pred, integration='trapz'):
     y_true = np.array(y_true, dtype=DTYPE)
     y_pred = np.array(y_pred, dtype=DTYPE)
 
-    rec = recall(y_true, y_pred)
-    prec = precision(y_true, y_pred)
+    n_uniques = np.unique(y_pred)
+    if n_uniques.size == 1:
+        raise ValueError('Rank of predicted values is ill-defined'
+                         ' because all elements are equal')
+    elif n_uniques.size < y_pred.size:
+        warning('some predicted elements have exactly the same value.'
+                ' output will most probably depend on the sorting'
+                ' method used. Here "%s"' % argsort_kind, UserWarning)
+
+    rec = recall(y_true, y_pred, argsort_kind=argsort_kind)
+    prec = precision(y_true, y_pred, argsort_kind=argsort_kind)
 
     if integration == 'trapz':
         if rec[0] != 0.:

bangmetric/tests/test_precision_recall.py
@@ -1,58 +1,185 @@
 """Test suite for the ``precision_recall`` module"""
 
+import warnings
 import numpy as np
 from pytest import raises
 from bangmetric import average_precision
+from bangmetric import precision
+from bangmetric import recall
 
-# XXX: test recall()
-# XXX: test precision()
 
-def test_length():
-    # should fail because y_pred and y_true don't have the same dimension
+def test_incompatible_input_dimensions_ap():
     y_pred = np.array([0.1, 0.2, 0.3])
     y_true = np.array([1., -1.])
     raises(AssertionError,
-           average_precision, y_true, y_pred)
+    average_precision, y_true, y_pred)
 
-def test_fail1():
-    # should fail because "dummy" is not a supported method
-    # of integration
+
+def test_incompatible_input_dimensions_precision():
+    y_pred = np.array([0.1, 0.2, 0.3])
+    y_true = np.array([1., -1.])
+    raises(AssertionError,
+    precision, y_true, y_pred)
+
+
+def test_incompatible_input_dimensions_recall():
+    y_pred = np.array([0.1, 0.2, 0.3])
+    y_true = np.array([1., -1.])
+    raises(AssertionError,
+    recall, y_true, y_pred)
+
+
+def test_wrong_integration_method():
     y_pred = np.array([0.87, 0.21, 0.35])
     y_true = np.array([1., -1., 1.])
     raises(AssertionError,
-           average_precision, y_pred, y_true, integration='dummy')
+    average_precision, y_true, y_pred, integration='dummy')
 
-#def test_pass1():
-    ## should pass because the predictions exactly match the
-    ## ground truth
-    #y_pred = np.array([1., 1., 1.])
-    #y_true = np.array([1., 1., 1.])
-    #ap = average_precision(y_pred, y_true)
-    #reference = 1.
-    #assert_almost_equal(ap, reference, decimal=6)
 
-#def test_pass2():
-    ## should pass because the predictions exactly match the ground truth
-    ## and that the integration method is irrelevant
-    #y_pred = np.array([1., 1., 1.])
-    #y_true = np.array([1., 1., 1.])
-    #ap = average_precision(y_pred, y_true, integration_type='voc2007')
-    #reference = 1.
-    #assert_almost_equal(ap, reference, decimal=6)
+def test_equal_predictions_error_precision():
+    y_pred = np.array([0.35, 0.35, 0.35])
+    y_true = np.array([1., -1., 1.])
+    raises(ValueError,
+    precision, y_true, y_pred)
 
-#def test_pass3():
-    ## should pass because the predictions are all wrong
-    #y_pred = np.array([1., 1., 1.])
-    #y_true = np.array([-1., -1., -1.])
-    #ap = average_precision(y_pred, y_true)
-    #reference = 0.
-    #assert_almost_equal(ap, reference, decimal=6)
 
-#def test_pass4():
-    ## should pass because the predictions are all wrong and that the
-    ## integration method is irrelevant
-    #y_pred = np.array([1., 1., 1.])
-    #y_true = np.array([-1., -1., -1.])
-    #ap = average_precision(y_pred, y_true, integration_type='voc2007')
-    #reference = 0.
-    #assert_almost_equal(ap, reference, decimal=6)
+def test_equal_predictions_error_recall():
+    y_pred = np.array([0.35, 0.35, 0.35])
+    y_true = np.array([1., -1., 1.])
+    raises(ValueError,
+    recall, y_true, y_pred)
+
+
+def test_equal_predictions_error_trapz():
+    y_pred = np.array([0.35, 0.35, 0.35])
+    y_true = np.array([1., -1., 1.])
+    raises(ValueError,
+    average_precision, y_true, y_pred, integration='trapz')
+
+
+def test_equal_predictions_error_voc2010():
+    y_pred = np.array([0.42, 0.42, 0.42])
+    y_true = np.array([1., -1., 1.])
+    raises(ValueError,
+    average_precision, y_true, y_pred, integration='voc2010')
+
+
+def test_equal_predictions_error_voc2007():
+    y_pred = np.array([0.07, 0.07, 0.07])
+    y_true = np.array([1., -1., 1.])
+    raises(ValueError,
+    average_precision, y_true, y_pred, integration='voc2007')
+
+
+def test_user_warning_precision():
+    y_pred = np.array([0.04, 0.04, 0.10])
+    y_true = np.array([1., -1., 1.])
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        precision(y_true, y_pred)
+        assert issubclass(w[-1].category, UserWarning)
+        assert "sorting method used" in str(w[-1].message)
+
+
+def test_user_warning_recall():
+    y_pred = np.array([0.04, 0.04, 0.10])
+    y_true = np.array([1., -1., 1.])
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        recall(y_true, y_pred)
+        assert issubclass(w[-1].category, UserWarning)
+        assert "sorting method used" in str(w[-1].message)
+
+
+def test_user_warning_trapz():
+    y_pred = np.array([0.04, 0.04, 0.10])
+    y_true = np.array([1., -1., 1.])
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        average_precision(y_true, y_pred, integration='trapz')
+        assert issubclass(w[-1].category, UserWarning)
+        assert "sorting method used" in str(w[-1].message)
+
+
+def test_user_warning_voc2010():
+    y_pred = np.array([0.04, 0.04, 0.10])
+    y_true = np.array([1., -1., 1.])
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        average_precision(y_true, y_pred, integration='voc2010')
+        assert issubclass(w[-1].category, UserWarning)
+        assert "sorting method used" in str(w[-1].message)
+
+
+def test_user_warning_voc2007():
+    y_pred = np.array([0.04, 0.04, 0.10])
+    y_true = np.array([1., -1., 1.])
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        average_precision(y_true, y_pred, integration='voc2007')
+        assert issubclass(w[-1].category, UserWarning)
+        assert "sorting method used" in str(w[-1].message)
+
+
+def test_perfect_pos_predictions_trapz():
+    y_pred = np.array([0.92, 0.99, 0.97])
+    y_true = np.array([1., 1., 1.])
+    ap = average_precision(y_true, y_pred, integration='trapz')
+    reference = 1.
+    assert abs(ap - reference) < 1e-6
+
+
+def test_perfect_pos_predictions_voc2010():
+    y_pred = np.array([0.92, 0.99, 0.97])
+    y_true = np.array([1., 1., 1.])
+    ap = average_precision(y_true, y_pred, integration='voc2010')
+    reference = 1.
+    assert abs(ap - reference) < 1e-6
+
+
+def test_non_trivial_precision_trapz():
+    y_pred = np.array([0.25, 0.45, 0.60, 0.90])
+    y_true = np.array([1., 1., 0., 1.])
+    prec = precision(y_true, y_pred)
+    reference = np.array([1., 0.5, 2. / 3., 0.75])
+    assert (np.abs(prec - reference)).sum() < 1e-6
+
+
+def test_non_trivial_recall_trapz():
+    y_pred = np.array([0.25, 0.45, 0.60, 0.90])
+    y_true = np.array([1., 1., 0., 1.])
+    rec = recall(y_true, y_pred)
+    reference = np.array([1. / 3., 1. / 3., 2. / 3., 1.])
+    assert (np.abs(rec - reference)).sum() < 1e-6
+
+
+def test_non_trivial_ap_trapz():
+    y_pred = np.array([0.25, 0.45, 0.60, 0.90])
+    y_true = np.array([1., 1., 0., 1.])
+    ap = average_precision(y_true, y_pred, integration='trapz')
+    reference = 0.7638888888888888
+    assert abs(ap - reference) < 1e-6
+
+
+def test_non_trivial_ap_voc2010():
+    y_pred = np.array([0.25, 0.45, 0.60, 0.90])
+    y_true = np.array([1., 1., 0., 1.])
+    ap = average_precision(y_true, y_pred, integration='voc2010')
+    reference = 0.8333333333333333
+    assert abs(ap - reference) < 1e-6
+
+
+def test_non_trivial_perfect_ap_trapz():
+    y_pred = np.array([0.82, 0.75, 0.60, 0.90])
+    y_true = np.array([1., 1., 0., 1.])
+    ap = average_precision(y_true, y_pred, integration='trapz')
+    reference = 1.
+    assert abs(ap - reference) < 1e-6
+
+
+def test_non_trivial_perfect_ap_voc2010():
+    y_pred = np.array([0.82, 0.75, 0.60, 0.90])
+    y_true = np.array([1., 1., 0., 1.])
+    ap = average_precision(y_true, y_pred, integration='voc2010')
+    reference = 1.
+    assert abs(ap - reference) < 1e-6