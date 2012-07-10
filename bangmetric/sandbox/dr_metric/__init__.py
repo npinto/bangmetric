bb20e257ad6d3e774f1b04e4eaf62b073326afc4
Nicolas Pinto <pinto@alum.mit.edu>
2012-07-10 17:50:03 -0400

    Moved sandbox around

--- bangmetric/sandbox/dr_metric/__init__.py
+++
@@ -1,5 +0,0 @@
-from imgdl_to_cftpfpfn import *
-from cftpfpfn_to_rec_prec import *
-from rec_prec_to_ap import *
-from imgdl_to_rec_prec_ap import *
-from accuracy import *
--- bangmetric/sandbox/m_vs_n_metric/__init__.py
+++
@@ -1,3 +0,0 @@
-from idc_ps import idc_ps
-from idc_rls import idc_rls
-from spear import spearman
