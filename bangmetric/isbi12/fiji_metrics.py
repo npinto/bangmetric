ca6967c2323e11890ea6b414f44ae88ef81caf6e
Nicolas Pinto <pinto@alum.mit.edu>
2012-07-26 16:41:55 -0400

    ENH: add warp2d wrapper from Fiji

--- bangmetric/isbi12/fiji_metrics.py
+++ bangmetric/isbi12/fiji_metrics.py
@@ -14,7 +14,7 @@ io.use_plugin('freeimage')
 
 from os import path, environ
 import inspect
-from tempfile import mkdtemp 
+from tempfile import mkdtemp
 from shutil import rmtree
 from skimage.exposure import rescale_intensity
 
@@ -29,14 +29,11 @@ base_path = path.split(path.abspath(__file__))[0]
 PX_SCRIPT_PATH = path.abspath(path.join(base_path, "px_err_cli.bsh"))
 RD_SCRIPT_PATH = path.abspath(path.join(base_path, "rd_err_cli.bsh"))
 WP_SCRIPT_PATH = path.abspath(path.join(base_path, "wp_err_cli.bsh"))
+WARP_CODE_PATH = path.abspath(path.join(base_path, "warp_cli.bsh"))
 
 # -- default paths to find the Fiji executable
 ENV_VAR = "FIJI_EXE_PATH"
 DEFAULT_FIJI_EXE_PATH = "/path/to/fiji-linux64"
-
-# XXX: hacky but we need it for now...
-assert environ.get(ENV_VAR) is not None, \
-        "You need to define the environment variable '%s'" % ENV_VAR
 
 
 def pixel_error(y_true, y_pred):
@@ -68,8 +65,39 @@ def warp_error(y_true, y_pred):
                    warping_error=True)
     return out['warping_error']
 
+ 
+def warp2d(y_true, y_pred, threshold=0.0, radius=20):
+    """XXX: docstring"""
 
-def _metrics(true_arr, pred_arr,
+    program, true_tmp_file, pred_tmp_file, tmpdir = \
+            _prepare_arrays(y_true, y_pred)
+
+    script = WARP_CODE_PATH
+
+    y_out_fname = path.join(tmpdir, 'out.tif')
+
+    cmdline = ("%s %s %s %s %s %s %s %s" % (
+        program, '--headless', script,
+        true_tmp_file, pred_tmp_file,
+        y_out_fname, threshold, radius
+    ))
+
+    return_code, stdout, stderr = _call_capture_output(cmdline)
+
+    if return_code == 0:
+        y_out = io.imread(y_out_fname, as_grey=True, plugin='freeimage')
+        y_out = (y_out > 0)
+    else:
+        print 'An error occured while executing command :'
+        print '%s' % cmdline
+        raise ExecError("return code %s" % return_code)
+
+    rmtree(tmpdir)
+
+    return y_out
+
+
+def _metrics(y_true, y_pred,
              pixel_error=True,
              rand_error=True,
              warping_error=True):
@@ -79,10 +107,10 @@ def _metrics(true_arr, pred_arr,
     Parameters
     ----------
 
-    `true_arr`: 2D array
+    `y_true`: 2D array
         ground truth annotations
 
-    `pred_arr`: 2D array
+    `y_pred`: 2D array
         predictions
 
     `pixel_error`: bool
@@ -107,50 +135,14 @@ def _metrics(true_arr, pred_arr,
     In the ISBI challenge, the "positive"
     """
 
-    # -- figuring out where the Fiji executable is
-    #log.warn("looking for %s environment variable..." % ENV_VAR)
-    if ENV_VAR not in environ:
-
-        log.warn("using default path '%s'..." % DEFAULT_FIJI_EXE_PATH)
-        if not path.exists(DEFAULT_FIJI_EXE_PATH):
-            raise ValueError(
-                "Could not find the path to the Fiji executable!"
-                "Please set the %s environment variable or set"
-                " the default path in %s" % (ENV_VAR,
-                path.abspath(inspect.getfile(inspect.currentframe()))))
-        else:
-            log.warn("Found '%s' !" % DEFAULT_FIJI_EXE_PATH)
-            FIJI_EXE_PATH = DEFAULT_FIJI_EXE_PATH
-
-    else:
-        FIJI_EXE_PATH = environ.get(ENV_VAR)
-
-    if not path.exists(FIJI_EXE_PATH):
-        raise ValueError("%s does not exist" % FIJI_EXE_PATH)
-
-    assert true_arr.ndim == 2
-    assert true_arr.ndim == pred_arr.ndim
-
     # -- special cases
     if not pixel_error and \
        not rand_error and \
        not warping_error:
         return {}
 
-    # -- making sure that the arrays are in the proper ranges and have the
-    # proper dtype
-    true_arr = 255 * (true_arr > 0).astype(np.uint8)
-    pred_arr = rescale_intensity(pred_arr.astype(np.float32))
-
-    # -- saving the arrays as tif images in a temp dir
-    tmpdir = mkdtemp()
-    true_tmp_file = path.join(tmpdir, 'true.tif')
-    pred_tmp_file = path.join(tmpdir, 'pred.tif')
-    io.imsave(true_tmp_file, true_arr, plugin='freeimage')
-    io.imsave(pred_tmp_file, pred_arr, plugin='freeimage')
-
-    # -- now we call the Fiji program to compute the metrics
-    program = FIJI_EXE_PATH
+    program, true_tmp_file, pred_tmp_file, tmpdir = \
+            _prepare_arrays(y_true, y_pred)
 
     metrics = {}
 
@@ -234,6 +226,48 @@ def _parse_stdout(output, metric='pixel'):
             metric_value = float(match.groups()[0])
 
     return metric_value
+
+
+def _prepare_arrays(y_true, y_pred):
+
+    # -- figuring out where the Fiji executable is
+    #log.warn("looking for %s environment variable..." % ENV_VAR)
+    if ENV_VAR not in environ:
+
+        if not path.exists(DEFAULT_FIJI_EXE_PATH):
+            raise ValueError(
+                "Could not find the path to the Fiji executable!"
+                "Please set the %s environment variable or set"
+                " the default path in %s" % (ENV_VAR,
+                path.abspath(inspect.getfile(inspect.currentframe()))))
+        else:
+            FIJI_EXE_PATH = DEFAULT_FIJI_EXE_PATH
+
+    else:
+        FIJI_EXE_PATH = environ.get(ENV_VAR)
+
+    if not path.exists(FIJI_EXE_PATH):
+        raise ValueError("%s does not exist" % FIJI_EXE_PATH)
+
+    assert y_true.ndim == 2
+    assert y_true.ndim == y_pred.ndim
+
+    # -- making sure that the arrays are in the proper ranges and have the
+    # proper dtype
+    y_true = 255 * (y_true > 0).astype(np.uint8)
+    y_pred = rescale_intensity(y_pred.astype(np.float32))
+
+    # -- saving the arrays as tif images in a temp dir
+    tmpdir = mkdtemp()
+    true_tmp_file = path.join(tmpdir, 'true.tif')
+    pred_tmp_file = path.join(tmpdir, 'pred.tif')
+    io.imsave(true_tmp_file, y_true, plugin='freeimage')
+    io.imsave(pred_tmp_file, y_pred, plugin='freeimage')
+
+    # -- now we call the Fiji program to compute the metrics
+    program = FIJI_EXE_PATH
+
+    return (program, true_tmp_file, pred_tmp_file, tmpdir)
 
 
 def _call_capture_output(cmdline, cwd=None, error_on_nonzero=True):
