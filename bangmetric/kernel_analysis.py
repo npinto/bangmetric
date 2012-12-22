ee803a635eee53a51d3b1f47ec8357c4ddac1e67
Nicolas Pinto <nicolas.pinto@gmail.com>
2012-12-22 15:21:24 +0100

    Update bangmetric/kernel_analysis.py
    FIX: revert numpy.sort() one-liner

--- bangmetric/kernel_analysis.py
+++ bangmetric/kernel_analysis.py
@@ -105,7 +105,8 @@ def kanalysis(X, Y_true, n_components='all', quantiles=DEFAULT_QUANTILES):
     l2_squared = X_ssq + X_ssq.T - 2. * np.dot(X, X.T)
 
     # Sort them
-    l2_squared_sorted = l2_squared.ravel().sort()
+    l2_squared_sorted = l2_squared.ravel()
+    l2_squared_sorted.sort()
 
     # ------------------------------------------------------------------------
     # -- Compute Kernel Analysis for each quantile
